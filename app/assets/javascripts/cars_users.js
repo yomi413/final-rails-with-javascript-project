541eea58d218c3554d5e20bcab8354018ba7b463
Yomaira Escano <yjescano@gmail.com>
2019-03-19 10:05:27 -0400

    Added turbolinks to event

--- app/assets/javascripts/cars_users.js
+++ app/assets/javascripts/cars_users.js
@@ -1,20 +1,23 @@
-$.ajax ({
-  type: 'GET',
-  url: '/cars.json',
-  success: function(cars) {
+$(document).on('turbolinks:load', function() {
+  $.ajax ({
+    type: 'GET',
+    url: '/cars.json',
+    success: function(cars) {
 
-    const carsHTML = cars.map(function(car) {
-      return `<tr>
-      <td>${car.parking_space.space_number}</td>
-      <td>${car.make}</td>
-      <td>${car.model}</td>
-      <td>${car.year}</td>
-      <td>${car.color}</td>
-      <td>${car.size}</td>
-      </tr>`
-    })
+      const carsHTML = cars.map(function(car) {
+        return `<tr>
+        <td>${car.parking_space.space_number}</td>
+        <td>${car.make}</td>
+        <td>${car.model}</td>
+        <td>${car.year}</td>
+        <td>${car.color}</td>
+        <td>${car.size}</td>
+        </tr>`
+      })
 
-    $('#user_cars').append(carsHTML);
-  }
+      $('#user_cars').append(carsHTML);
+    }
 
+  })
 })
+
