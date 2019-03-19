16467500f3a71e905354a090e9f67881d995ec7a
Yomaira Escano <yjescano@gmail.com>
2019-03-19 13:01:59 -0400

    Added AJAX for the cars/show page

--- app/assets/javascripts/cars_users.js
+++ app/assets/javascripts/cars_users.js
@@ -1,4 +1,7 @@
+const carsURL = window.location.pathname
+
 $(document).on('turbolinks:load', function() {
+  
   $.ajax ({
     type: 'GET',
     url: '/cars.json',
@@ -20,4 +23,25 @@ $(document).on('turbolinks:load', function() {
 
   })
 })
+
+$(document).on('turbolinks:load', function() {
+  $.ajax ({
+    type: 'GET',
+    url: `${carsURL}.json`,
+    success: function(car) {
+
+      const carInfo = `
+        <p><strong>Car:</strong>${car.your_car}</p>
+        <p><strong>Color:</strong>${car.color}</p>
+        <p><strong>Size:</strong>${car.size}</p>
+        <p><strong>Parking Space Number:</strong>${car.parking_space.space_number}</p>
+        `
+
+      $('#car_info').append(carInfo);
+
+    }
+  })
+})
+
+// <%= link_to "Edit Car", edit_car_path(@car) %> | <%= link_to "Delete Car", @car, :method => :delete %>
 
