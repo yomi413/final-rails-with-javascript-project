0e2faaa77fe7d9dec146dbc04ebcddab6b79a420
Yomaira Escano <yjescano@gmail.com>
2019-03-18 22:32:55 -0400

    Attempt at implementing AJAX and jQuery to access cars/index view page

--- app/assets/javascripts/cars_users.js
+++ app/assets/javascripts/cars_users.js
@@ -1,1 +1,20 @@
-alert('This file is now connected!')
+$.ajax ({
+  type: 'GET',
+  url: '/cars.json',
+  success: function(cars) {
+
+    const carsHTML = cars.map(function(car) {
+      return `<tr>
+      <td>${car.parking_space.space_number}</td>
+      <td>${car.make}</td>
+      <td>${car.model}</td>
+      <td>${car.year}</td>
+      <td>${car.color}</td>
+      <td>${car.size}</td>
+      </tr>`
+    })
+
+    $('#user_cars').append(carsHTML);
+  }
+
+})
