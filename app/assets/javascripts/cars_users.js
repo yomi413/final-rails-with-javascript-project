9b7a7e2a576236f85cae2ba68a32b928c9ad499c
Yomaira Escano <yjescano@gmail.com>
2019-03-19 22:31:30 -0400

    Adjusted the cars/index jQuery call

--- app/assets/javascripts/cars_users.js
+++ app/assets/javascripts/cars_users.js
@@ -1,27 +1,30 @@
-const carsURL = window.location.pathname
+
 
 $(document).on('turbolinks:load', function() {
+  const carsURL = window.location.pathname;
+  if (carsURL === '/cars') {
+    $.ajax ({
+      type: 'GET',
+      url: `${carsURL}.json`,
+      success: function(cars) {
+
+        const carsHTML = cars.map(function(car) {
+          return `<tr>
+          <td>${car.parking_space.space_number}</td>
+          <td>${car.make}</td>
+          <td>${car.model}</td>
+          <td>${car.year}</td>
+          <td>${car.color}</td>
+          <td>${car.size}</td>
+          </tr>`
+        })
+
+        $('#user_cars').append(carsHTML);
+      }
+
+    })
+  }
   
-  $.ajax ({
-    type: 'GET',
-    url: '/cars.json',
-    success: function(cars) {
-
-      const carsHTML = cars.map(function(car) {
-        return `<tr>
-        <td>${car.parking_space.space_number}</td>
-        <td>${car.make}</td>
-        <td>${car.model}</td>
-        <td>${car.year}</td>
-        <td>${car.color}</td>
-        <td>${car.size}</td>
-        </tr>`
-      })
-
-      $('#user_cars').append(carsHTML);
-    }
-
-  })
 })
 
 $(document).on('turbolinks:load', function() {
@@ -31,16 +34,20 @@ $(document).on('turbolinks:load', function() {
     success: function(car) {
 
       const carInfo = `
-        <p><strong>Car:</strong>${car.your_car}</p>
-        <p><strong>Color:</strong>${car.color}</p>
-        <p><strong>Size:</strong>${car.size}</p>
-        <p><strong>Parking Space Number:</strong>${car.parking_space.space_number}</p>
+        <p><strong>Car:</strong> ${car.make} ${car.model} ${car.year}</p>
+        <p><strong>Color:</strong> ${car.color}</p>
+        <p><strong>Size:</strong> ${car.size}</p>
+        <p><strong>Parking Space Number:</strong> ${car.parking_space.space_number}</p>
         `
 
       $('#car_info').append(carInfo);
 
     }
   })
+})
+
+$(document).on('turbolinks:load', function() {
+
 })
 
 // <%= link_to "Edit Car", edit_car_path(@car) %> | <%= link_to "Delete Car", @car, :method => :delete %>
