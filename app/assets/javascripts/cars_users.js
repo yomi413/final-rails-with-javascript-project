5ccf7bc8d2b51cd201dc212dd933dbc032b83a86
Yomaira Escano <yjescano@gmail.com>
2019-03-19 23:40:38 -0400

    Modified to include jQuery for cars/new file

--- app/assets/javascripts/cars_users.js
+++ app/assets/javascripts/cars_users.js
@@ -24,32 +24,43 @@ $(document).on('turbolinks:load', function() {
         $('#user_cars').append(carsHTML);
       }
     })
-  } else if (carsURL === '/cars')
+  } else if (isNumeric(carsURL)) {
+    $.ajax ({
+      type: 'GET',
+      url: `${carsURL}.json`,
+      success: function(car) {
+
+        const carInfo = `
+          <p><strong>Car:</strong> ${car.make} ${car.model} ${car.year}</p>
+          <p><strong>Color:</strong> ${car.color}</p>
+          <p><strong>Size:</strong> ${car.size}</p>
+          <p><strong>Parking Space Number:</strong> ${car.parking_space.space_number}</p>`
+
+        $('#cars').append(carInfo);
+
+      }
+    })
+  } else if (carsURL === '/cars/new') {
+    $(function () {
+      $('form').submit(function(event) {
+        event.preventDefault();
+        const values = $(this).serialize();
+        const posting = $.post('/cars.json', values);
+        posting.done(function(data) {
+          const car = data;
+          $('#new_parking').text(`${car.parking_space.space_number}`);
+          $('new_make').text(`${car.make}`);
+          $('new_model').text(`${car.model}`);
+          $('new_year').text(`${car.year}`);
+          $('new_color').text(`${car.color}`);
+          $('new_size').text(`${car.size}`)
+        })
+      })
+    })
+  }
   
 })
 
-$(document).on('turbolinks:load', function() {
-  $.ajax ({
-    type: 'GET',
-    url: `${carsURL}.json`,
-    success: function(car) {
-
-      const carInfo = `
-        <p><strong>Car:</strong> ${car.make} ${car.model} ${car.year}</p>
-        <p><strong>Color:</strong> ${car.color}</p>
-        <p><strong>Size:</strong> ${car.size}</p>
-        <p><strong>Parking Space Number:</strong> ${car.parking_space.space_number}</p>
-        `
-
-      $('#car_info').append(carInfo);
-
-    }
-  })
-})
-
-$(document).on('turbolinks:load', function() {
-
-})
 
 // <%= link_to "Edit Car", edit_car_path(@car) %> | <%= link_to "Delete Car", @car, :method => :delete %>
 
