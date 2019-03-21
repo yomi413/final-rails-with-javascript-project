4586600920fac720f16e4a49a6e47e9df182d438
Yomaira Escano <yjescano@gmail.com>
2019-03-20 22:03:13 -0400

    Modified the users controller/show route to account for json rendering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @user }
+    end
   end
 
   private
