9f992d9e6d58075b5ce4d72ee9bb12fe08b353ec
yomi413 <yjescano@gmail.com>
2019-04-15 20:47:17 -0400

    Added new sorting feature to the js file

--- app/assets/javascripts/cars_users.js
+++ app/assets/javascripts/cars_users.js
@@ -6,25 +6,26 @@ $(document).on('turbolinks:load', function() {
       cars.map((carJSON) => {
         const car = new Car(carJSON);
 
-        const carsHTML = car.toHTML();
-        
+        // const carsHTML = car.toHTML();
+
         if (!car.user.admin) {
           car.registeredCars();
-        } 
+        }
 
         car.userCars();
       })
+      // console.log(car)
     })
   } else if (/cars\/\d+$/.test(url)) {
     $.get(`${url}.json`, function(showCar) {
-      
-      const singleCar = 
+
+      const singleCar =
       `<p><strong>Car:</strong> ${showCar.make} ${showCar.model} (${showCar.year})</p>
       <p><strong>Color:</strong> ${showCar.color}</p>
       <p><strong>Size:</strong> ${showCar.size}</p>
       <p><strong>Parking Space Number:</strong> ${showCar.parking_space.space_number}</p>`
 
-      return $('#cars').append(singleCar);  
+      return $('#cars').append(singleCar);
     })
   } else if (url === '/cars/new') {
     $('form').submit(function(event) {
@@ -47,4 +48,31 @@ $(document).on('turbolinks:load', function() {
       user.parkingSpaceList();
     })
   }
+  $('#sorted_cars').click(function() {
+    $.get(`${url}.json`, function(cars) {
+
+      const sortedCars = cars.sort(function(firstCar, secondCar) {
+        let x = firstCar.make.toLowerCase();
+        let y = secondCar.make.toLowerCase();
+        if (x < y) {
+          return -1
+        } else if (x > y) {
+          return 1
+        }
+        return 0
+      })
+
+      const sortedCar = sortedCars.map(car => {
+        return `<tr>
+          <td>${car.parking_space.space_number}</td>
+          <td>${car.make}</td>
+          <td>${car.model}</td>
+          <td>${car.year}</td>
+          <td>${car.color}</td>
+          <td>${car.size}</td>
+        </tr>`
+      })
+      $("#user_cars").replaceWith(`${sortedCar}`)
+    })
+  })
 })
